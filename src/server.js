bd95751bad1f8677828a8d2bd63e6797d1e4abdd
Tadeu Rangel <tadeurangel@MacBook-Air.local>
2019-04-10 20:29:12 -0300

    estrutura completa

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
+
 const app = express();
 app.use(cors());
 
-const server = require('http').server(app);
+const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 io.on("connection", socket => {
@@ -31,4 +32,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'temp')));
 
 app.use(require('./routes'));
 
-server.listen(process.env.PORT || 3333);
+server.listen(process.env.PORTgit || 3333);
